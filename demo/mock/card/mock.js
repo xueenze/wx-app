a97da1b39fea00f35c94681ad800db270cb048d6
enzexue <kameleon@126.com>
2018-03-26 12:00:22 +0800

    增加卡片组件

---
+++ demo/mock/card/mock.js
@@ -0,0 +1,7 @@
+export const mock = [{
+    id: 1,
+    imageUrl: "/images/card/card-demo.jpg",
+    title: "我是标题",
+    subTitle: "我是副标题",
+    addDate: "2018-10-01"
+}];
--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -1,24 +1,10 @@
+import { mock } from '../../mock/card/mock';
+
 const App = getApp();
 
 Page({
     data: {
-        cards: [{
-                id: 1,
-                imageUrl: "../../images/card/card-demo.jpg"
-            },
-            {
-                id: 2,
-                imageUrl: "../../images/card/card-demo.jpg"
-            },
-            {
-                id: 3,
-                imageUrl: "../../images/card/card-demo.jpg"
-            },
-            {
-                id: 4,
-                imageUrl: "../../images/card/card-demo.jpg"
-            },
-        ],
+        cards: mock,
         windowWidth: App.systemInfo.windowWidth,
         windowHeight: App.systemInfo.windowHeight
     },
