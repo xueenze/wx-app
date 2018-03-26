f7fb18a37776d97b57d6379b53a67a014f138335
enzexue <kameleon@126.com>
2018-03-26 18:17:18 +0800

    修改模拟数据

--- demo/mock/card/mock.js
+++ demo/mock/card/mock.js
@@ -1,7 +1,31 @@
 export const mock = [{
     id: 1,
     imageUrl: "/images/card/card-demo.jpg",
-    title: "我是标题",
-    subTitle: "我是副标题",
+    title: "【刺激战场】浅析",
+    subTitle: "现象级手游",
+    addDate: "2018-10-01"
+},{
+    id: 2,
+    imageUrl: "/images/card/card-demo.jpg",
+    title: "【微信跳一跳】浅析",
+    subTitle: "微信小游戏",
+    addDate: "2018-10-01"
+},{
+    id: 3,
+    imageUrl: "/images/card/card-demo.jpg",
+    title: "【小程序】浅析",
+    subTitle: "微信小程序",
+    addDate: "2018-10-01"
+},{
+    id: 4,
+    imageUrl: "/images/card/card-demo.jpg",
+    title: "【阅文集团】2017年度财报",
+    subTitle: "财报分析",
+    addDate: "2018-10-01"
+},{
+    id: 5,
+    imageUrl: "/images/card/card-demo.jpg",
+    title: "【得到】浅析",
+    subTitle: "应用分析",
     addDate: "2018-10-01"
 }];
