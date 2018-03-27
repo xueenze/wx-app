5481d4020ae8ff76c89791f0fb37a08ef8f5a02d
enzexue <kameleon@126.com>
2018-03-27 18:57:00 +0800

    修改图片

--- demo/mock/aboutme/mock.js
+++ demo/mock/aboutme/mock.js
@@ -2,7 +2,7 @@ export const mock = [{
     id: 1,
     date: "2011-09-01",
     imgUrl: "../../../images/aboutme/time1.jpg",
-    content: "我是内容我是内容我是内容，我是内容我是内容我是内容，我是内容我是内容我是内容",
+    content: "辽宁大连东北银，毕业于四川大学 软件工程专业",
     shotTime: "2011-10-11"
 }, {
     id: 2,
